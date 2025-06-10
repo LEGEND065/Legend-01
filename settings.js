8ba568d8a219c3e8df602e2b9a3971b56e380a29
LEGEND065 <onyemaekeoma24@gmail.com>
2025-06-10 05:54:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Legend', // Your name
-  ownerNumber: '9073373133 9132172314', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '9073373133', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
